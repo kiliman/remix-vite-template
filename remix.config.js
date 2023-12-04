bc48128a28d684575be41d3c79ecf657705fe10f
kiliman <kiliman@gmail.com>
2023-12-04 18:30:47 -0500

    📦 Add remix-flat-routes

--- remix.config.js
+++ remix.config.js
@@ -1,9 +1,9 @@
+import { flatRoutes } from 'remix-flat-routes'
 /** @type {import('@remix-run/dev').AppConfig} */
 export default {
-  ignoredRouteFiles: ['**/.*'],
+  ignoredRouteFiles: ['**/*'],
   serverModuleFormat: 'esm',
-  // appDirectory: "app",
-  // assetsBuildDirectory: "public/build",
-  // publicPath: "/build/",
-  // serverBuildPath: "build/index.js",
+  routes: async defineRoutes => {
+    return flatRoutes('routes', defineRoutes)
+  },
 }
