10f7372bc85ddd4bb632fe2965fea0bd87076884
kiliman <kiliman@gmail.com>
2023-12-07 11:39:47 -0500

    💄 Output Vite information to console during startup
    Since we're using a custom Express server, we don't get the Vite dev server output during startup. This just emulates what is printed.

--- server.mjs
+++ server.mjs
@@ -1,12 +1,21 @@
+import * as fs from 'node:fs'
 import { createRequestHandler } from '@remix-run/express'
 import { installGlobals } from '@remix-run/node'
+import chalk from 'chalk'
 import express from 'express'
 import morgan from 'morgan'
 
+const start = Date.now()
+
 // @remix-run/dev module is only available in development
 let remixDev
+let viteVersion
 
 if (process.env.NODE_ENV !== 'production') {
+  // get the vite version from the vite package.json
+  viteVersion = JSON.parse(
+    fs.readFileSync('node_modules/vite/package.json'),
+  ).version
   remixDev = await import('@remix-run/dev')
 }
 
@@ -23,6 +32,8 @@ const app = express()
 if (vite) {
   app.use(vite.middlewares)
 } else {
+  // add morgan here for production only
+  // dev uses morgan plugin, otherwise it spams the console with HMR requests
   app.use(morgan('tiny'))
   app.use(
     '/build',
@@ -42,4 +53,28 @@ app.all(
 )
 
 const port = 3000
-app.listen(port, '0.0.0.0', () => console.log('http://localhost:' + port))
+app.listen(port, '0.0.0.0', () => {
+  if (process.env.NODE_ENV === 'production') {
+    console.log('http://localhost:' + port)
+  } else {
+    // since we're using a custom server, emulate what vite dev server prints
+
+    const elapsed = Date.now() - start
+
+    console.log(
+      `  ${chalk.greenBright.bold('VITE')} ${chalk.green(
+        `v${viteVersion}`,
+      )} ready in ${chalk.bold(elapsed)} ms`,
+    )
+    console.log()
+    console.log(
+      `  ${chalk.greenBright.bold('➜')}  ${chalk.bold('Local:')}   ${chalk.cyan(
+        'http://localhost:' + port,
+      )}`,
+    )
+    console.log()
+    console.log(`  ${chalk.yellow('⚠️  Remix support for Vite is unstable')}`)
+    console.log(`  ${chalk.yellow('   and not recommended for production')}`)
+    console.log()
+  }
+})
