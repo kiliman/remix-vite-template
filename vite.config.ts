c0419b62a8934166aaab1c0e2c787da666a612d4
kiliman <kiliman@gmail.com>
2023-12-19 08:34:47 -0500

    🧹 Update import sort order via eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { unstable_vitePlugin as remix } from '@remix-run/dev'
 import morgan from 'morgan'
+import { remixDevTools } from 'remix-development-tools/vite'
 import { defineConfig, type ViteDevServer } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import remixConfig from './remix.config'
-import { remixDevTools } from 'remix-development-tools/vite'
 
 export default defineConfig({
   plugins: [
