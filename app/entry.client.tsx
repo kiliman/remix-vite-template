9a61a5a50c51bb6d0cbeb92da03025683ef27aa6
kiliman <kiliman@gmail.com>
2023-12-22 11:13:34 -0600

    📦 Update isbot to latest and include entry.client/server

---
+++ app/entry.client.tsx
@@ -0,0 +1,12 @@
+import { startTransition, StrictMode } from 'react'
+import { hydrateRoot } from 'react-dom/client'
+import { RemixBrowser } from '@remix-run/react'
+
+startTransition(() => {
+  hydrateRoot(
+    document,
+    <StrictMode>
+      <RemixBrowser />
+    </StrictMode>,
+  )
+})
