4fc729ea86e4f4f2e8690b3eabaf7e5f61ebe98c
broisnischal <neeswebservice@gmail.com>
2024-01-27 13:41:49 +0545

    test: testing for build

--- app/routes/resources+/healthcheck.tsx
+++ app/routes/resources+/healthcheck.tsx
@@ -3,3 +3,5 @@ import { type LoaderFunctionArgs } from '@remix-run/node'
 export async function loader({ request }: LoaderFunctionArgs) {
   return new Response('OK')
 }
+
+// why build not triggering
